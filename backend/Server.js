c87f9324be17b70444cabd472771d77f7c4fca80
Ankita Yadav <ankitajyadav02@gmail.com>
2026-06-01 13:21:13 +0530

    LC

--- backend/Server.js
+++ backend/Server.js
@@ -4,23 +4,29 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 
 dotenv.config();
+
+// Connect Database
 connectDB();
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Test Route
 app.get("/", (req, res) => {
   res.send("API Running");
 });
 
+// Routes
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
 
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 
+// Server Port
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
